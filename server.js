f1e9fa17f2d21b8f64dd596da1bf6ce4581a5e5c
Les James <lesjames_nz@hotmail.com>
2024-07-05 10:48:41 +1200

    randomPersonDetails

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bp = require('body-parser');
+const Faker = require('faker/lib');
 const app = express();
 const PORT = 10500
+let faker = new Faker();
+
 
 app.use(bp.json());
 
@@ -11,5 +14,15 @@ app.get("/", (req, res) => {
     res.send({message: 'hello'});
 });
 
-
+app.get('/randomPersonDetails', (req, res) => {
+    let firstname = faker.name.firstName();
+    let surname = faker.name.lastName();
+    res.send(
+        {
+            givenName: firstname,
+            surname: surname,
+            email: `${firstname}.${surname}1234@gmail.com`
+        }
+    )
+})
 
